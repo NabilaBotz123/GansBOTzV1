940c10ebc8c0b732195e4cfe1e0966b3ab1e837c
NabilaBotz123 <108862980+NabilaBotz123@users.noreply.github.com>
2022-07-11 12:56:02 +0700

    Update menui.js

--- plugins/menui.js
+++ plugins/menui.js
@@ -169,7 +169,7 @@ let replace = {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-let url = `https://telegra.ph/file/01a5b3b1fc41ccaa284f8.png`
+let url = `https://telegra.ph/file/14ddef4c94f8ba4a2e237.jpg`
 let res = await fetch(url)
 let buffer = await res.buffer()
 let message = await prepareWAMessageMedia({ image: buffer }, { upload: conn.waUploadToServer })
